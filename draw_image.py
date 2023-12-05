9f00c39dff92f5fa64df8645a26ad4e9330cb8f9
Manolis Tzagakis <sxoinatzag@hotmail.gr>
2023-12-05 16:41:58 +0200

    small improvements

--- draw_image.py
+++ draw_image.py
@@ -11,7 +11,7 @@ def line_betw(image, results, n1, n2, w, h):
 
 
 def draw_skeleton(image, results, w, h):
-    feat_list = [11, 12, 13, 14, 15, 16, 23, 24, 25, 26, 27, 28]
+    feat_list = [0, 7, 8, 11, 12, 13, 14, 15, 16, 23, 24, 25, 26, 27, 28]
     for i in feat_list:
         cv2.circle(image, get_coord(results.pose_landmarks.landmark[i], w, h), 5, (0,255,0), 2)
     lines = ((11,12), (12,14), (14,16), (11,13), (13,15), (11,23), (24,12), (23,24), (25,23), (25,27), (24,26), (26,28))
