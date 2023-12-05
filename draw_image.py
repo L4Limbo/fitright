a287caf8ec8e449259171ddb2ac0eeeeea2d6566
Manolis Tzagakis <sxoinatzag@hotmail.gr>
2023-12-05 22:29:07 +0200

    calculate center of head

--- draw_image.py
+++ draw_image.py
@@ -10,8 +10,17 @@ def line_betw(image, results, n1, n2, w, h):
     return None
 
 
+def center_of_head(image, results, w, h):
+    le_x, le_y = get_coord(results.pose_landmarks.landmark[7],w, h)
+    re_x, re_y = get_coord(results.pose_landmarks.landmark[8],w, h)
+    
+    return (le_x+re_x)//2, (le_y +re_y)//2
+
+
 def draw_skeleton(image, results, w, h):
-    feat_list = [0, 7, 8, 11, 12, 13, 14, 15, 16, 23, 24, 25, 26, 27, 28]
+    feat_list = [11, 12, 13, 14, 15, 16, 23, 24, 25, 26, 27, 28]
+    mhx, mhy = center_of_head(image, results, w, h)
+    cv2.circle(image, (mhx, mhy), 5, (0,255,0), 2)
     for i in feat_list:
         cv2.circle(image, get_coord(results.pose_landmarks.landmark[i], w, h), 5, (0,255,0), 2)
     lines = ((11,12), (12,14), (14,16), (11,13), (13,15), (11,23), (24,12), (23,24), (25,23), (25,27), (24,26), (26,28))
