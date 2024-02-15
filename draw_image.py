b1589c566274831da6305ca96ebde35faa2cbfb6
L4Limbo <anestis.mitakidis@gmail.com>
2024-02-15 17:46:24 +0200

    push up counter init

--- draw_image.py
+++ draw_image.py
@@ -16,10 +16,10 @@ def center_of_line(image, results, n1, n2, w, h):
     re_x, re_y = get_coord(results.pose_landmarks.landmark[n2],w, h)
     
     return (le_x+re_x)//2, (le_y +re_y)//2
-
+ 
 
 def draw_skeleton(image, results, w, h):
-    feat_list = [11, 12, 13, 14, 15, 16, 23, 24, 25, 26, 27, 28]
+    feat_list = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 23, 24, 25, 26, 27, 28,]
     mhx, mhy = center_of_line(image, results, 7, 8, w, h)
     msx, msy = center_of_line(image, results, 11, 12, w, h)
 
@@ -30,7 +30,9 @@ def draw_skeleton(image, results, w, h):
 
     for i in feat_list:
         cv2.circle(image, get_coord(results.pose_landmarks.landmark[i], w, h), 5, (0,255,0), 2)
-    lines = ((11,12), (12,14), (14,16), (11,13), (13,15), (11,23), (24,12), (23,24), (25,23), (25,27), (24,26), (26,28))
+        cv2.putText(image, str(i), get_coord(results.pose_landmarks.landmark[i], w, h), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    
+    lines = ((11,12), (12,14), (14,16), (11,13), (13,15), (11,23), (24,12), (23,24), (25,23), (25,27), (24,26), (26,28), (20,18), (18,16), (16,20), (15,19), (19,17), (15,17))
     for points in lines:
         line_betw(image, results, points[0], points[1], w, h)                
     return None
--- main.py
+++ main.py
@@ -12,12 +12,15 @@ if __name__ == "__main__":
     mp_holistic = mp.solutions.holistic
 
     cap = cv2.VideoCapture(0)
+    
 
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
         
         while cap.isOpened():
 
             ret, frame = cap.read()
+            
+            frame = cv2.flip(frame, 1)
             image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             h, w = image.shape[:2]
             results = holistic.process(image)
